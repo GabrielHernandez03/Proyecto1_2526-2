17114fa6b8f22186f7db539655637f102fbc6edc
Gabriel <GabrielHernandez03>
2026-02-22 23:23:56 -0400

    proteina 1

---
+++ src/proyectohernandezgabriel/Proteina.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectohernandezgabriel;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Proteina {
+    String nombre;
+    int peso;
+    
+    
+}
