1bbf8c3b00eae5a6d951f443b8abbeebf583805c
Gabriel <GabrielHernandez03>
2026-02-22 23:24:36 -0400

    proteina 2

--- src/proyectohernandezgabriel/Proteina.java
+++ src/proyectohernandezgabriel/Proteina.java
@@ -9,8 +9,36 @@ package proyectohernandezgabriel;
  * @author Gabriel
  */
 public class Proteina {
-    String nombre;
-    int peso;
+    private String nombre;
+    private int peso;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the peso
+     */
+    public int getPeso() {
+        return peso;
+    }
+
+    /**
+     * @param peso the peso to set
+     */
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
     
     
 }
